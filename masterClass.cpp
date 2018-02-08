5730a54546bb963b2a8a7cc9cc7918c783e0c5dd
mshoffn <michaelashoffner@hotmail.com>
2018-02-08 15:31:14 -0500

    Adding some files for stuff

---
+++ masterClass.cpp
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+#include "masterClass.h"
+
+MasterClass::MasterClass() {
+
+	//ask user whether they wanna start a server or join
+	
+	//if start
+	//	fork and start server running seperately
+	//
+	//else
+	//	try and join requested server
+	//	if success
+	//		go down and do the client thing
+	//	else
+	//		fork and start server running seperately
+	//
+	//Do the client thing
+
+}
+
+void MasterClass::server() {
+
+}
+
+void MasterClass::client() {
+
+}
+
+void MasterClass::scrapAll() {
+
+}
---
+++ masterClass.h
@@ -0,0 +1,17 @@
+/*
+ *
+ */
+
+#include <cstdio>
+
+using namespace std;
+
+class MasterClass {
+	public:
+		MasterClass();
+		void server();
+		void client();
+		void scrapAll(); //destructor
+	private:
+
+};
