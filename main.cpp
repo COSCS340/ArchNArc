fdcc416bd8ca8ac6d900e794bab8b6616ff34ba2
Ruethus <redingtonknoxtn@gmail.com>
2018-02-12 14:02:52 -0500

    Started an actual main file

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "fields.h"
+#include <cstdio>
+using namespace std;
+
+unsigned char cinit();
+unsigned char crun();
+unsigned char cterm();
+unsigned char sinit();
+unsigned char srun();
+unsigned char sterm();
+
+int spid; // PID of the server, only set to non-zero if this is the client that launched the server
+
+int main(int argc,char** args) {
+	spid=0;
+	IS* in = new_inputstruct(NULL);
+	printf("Welcome to Archery & Arcana, player.\nWill you be connecting to a server (0) or starting your own (1)?\n");
+	getline(in);
+	if(strcmp(in->fields[0],"0")) {
+		spid = fork();
+		if(!spid) {
+			sinit();
+			srun();
+			sterm();
+		}
+	}
+	cinit();
+	crun();
+	cterm();
+	int i;
+	if(spid) wait(&i);
+]
