0ae251c4426fa37d6892304a443b2a30b136e328
RagnarokerXIII <35303825+RagnarokerXIII@users.noreply.github.com>
2018-03-05 13:57:34 -0500

    Character creation loop

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string>
 #include "fields.h"
 #include "entities.h"
 using namespace std;
@@ -22,10 +23,61 @@ int main(int argc,char** args) {
 	if(cinit()) return 1;
 	if(crun()) return 1;
 	if(cterm()) return 1;
-	int i;
 
-	Entity ne;
-
+	Entity*[7] party;
+	int place = 0;
+	char ans;
+	string name;
+	string temp;
+	byte class;
+	for (int i = 0; i < 7; i++)
+		party[i] = NULL;
+	while (true) {
+		while (true){
+			printf("Would you like to create a new character?(y/n)");
+			cin >> ans;
+			ans = tolower(ans);
+			if (ans == 'y' || ans == 'n')
+				break;
+			printf("Error, incorrect input. Try Again.");
+		}
+		if (ans == 'y'){
+			party[place] = new Entity;
+			printf ("Enter the character's name: ");
+			cin >> name;
+			printf("What class would you like to be? Please choose one of the following.\n");
+			printf(" ----------------------------\n");
+			for(int i = 0; i < NUM_JOBS; i++) {
+				printf("| %5d %-20s |\n", i+1, JOBS[i].c_str());
+				//print job descriptions?
+			}
+			printf(" ----------------------------\n");
+			while(true) {
+				printf(" >> ");
+				getline(std::cin, temp);
+				for (class = 0; class < 6; class++)
+					if (temp == JOBS[class])
+						break;
+				if (class == 6){ //bad input
+					printf("I don't recognize that. Please try again.\n");
+				}
+			}
+			party[place]->setUpChar(name, (byte) 0, class);
+		}
+		while (true) {
+			printf ("Would you like to load a character?(y/n)");
+			cin >> ans;
+			ans = tolower(ans);
+			if (ans == 'y' || ans == 'n')
+				break;
+			printf("Error, incorrect input. Try Again.");
+		}
+		if (ans == 'y'){
+			printf("Enter your character's name: ");
+			getline(cin, name);
+			
+		}
+	}
 	jettison_inputstruct(in);
 	if(spid) wait(&i);
 }
