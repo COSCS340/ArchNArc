58966fdd6eaa7ce065c0a7f9f54456a34bb90e16
RagnarokerXIII <35303825+RagnarokerXIII@users.noreply.github.com>
2018-03-28 13:46:22 -0400

    Added very broken Skills for the gambler class.

--- entities.cpp
+++ entities.cpp
@@ -470,11 +470,11 @@ void Entity::skill() {
 	string temp;	
 	do {
 		printf("Which skill does %s use?\n", name.c_str());
-		printf(" --------------------------------------\n");
+		printf(" ----------------------------\n");
 		for(int i = 0; i < SKILLS[jobNum]; i++) {
 			printf("| %5d %-20s |\n", i+1, SKILL_LIST[jobNum][i].c_str());
 		}
-		printf(" --------------------------------------\n");
+		printf(" ----------------------------\n");
 		while(true) {
 			flag = 1;
 			printf(" >> ");
@@ -908,18 +908,161 @@ void coinFlip(Entity* ePtr){
 		printf("Heads. You deal %d damage to all Enemies in this room\n", payment*5);
 		for (int i = 0; i < ePtr->room->inRoom.size(); i++)
 			if (ePtr->room->inRoom[i]->npc)
-				ePtr->room->inRoom[i]->cur_hp -= 5;
+				ePtr->room->inRoom[i]->cur_hp -= payment*5;
 	}
 	else{
 		printf("Tails. You deal %d damage to yourself\n", payment*5);
-			ePtr->cur_hp -= 5;
+			ePtr->cur_hp -= payment*5;
 	}
 	ePtr->cooldown = 20;
 }
 
 void multislash(Entity* ePtr) {
-	return;
+	Entity* ePtr1;
+	int target, size, flag, strSize;
+	string temp;
+	size = ePtr->room->inRoom.size();	
+	do {
+		printf("Who does %s attack?\n", ePtr->name.c_str());
+		printf(" --------------------------------------\n");
+		for(int i = 0; i < size; i++) {
+			if (ePtr->room->inRoom[i]->cur_hp < 1){
+				strSize = ePtr->room->inRoom[i]->name.size();
+				ePtr->room->inRoom[i]->name += " dead";
+				ePtr1 = ePtr->room->inRoom[i];
+				printf("| %5d %-30s |\n", i+1, ePtr1->name.c_str());
+				ePtr->room->inRoom[i]->name.resize(strSize);
+			}
+			else {
+				ePtr1 = ePtr->room->inRoom[i];
+				printf("| %5d %-30s |\n", i+1, ePtr1->name.c_str());
+			}
+		}
+		printf(" --------------------------------------\n");
+		while(true) {
+			flag = 1;
+			printf(" >> ");
+			getline(cin, temp);
+			target = atoi(temp.c_str()) - 1;
+			if(target >= 0 && target < size) { 
+				break;
+			} 
+			else {
+				for(int i = 0; temp[i] != '\0'; i++) {
+					temp[i] = tolower(temp[i]);
+				}
+				for (target = 0; target < size; target++) {
+					if (temp == ePtr->room->inRoom[target]->name) {
+						flag = 0;
+						break;
+					}
+				}
+				if(flag == 0) {
+					break;
+				}
+				if (target == size){ //bad input
+					printf("I don't recognize that. Please try again.\n");
+				}
+			}
+		}
+	}while (target == size);
+	ePtr1 = ePtr->room->inRoom[target];
+	int payment, strikes, odds;
+	do {
+		printf("How much gold do you pay to Weddench?\n >> ");
+		cin >> payment;
+		if (payment < 0 || payment > ePtr->cash)
+			printf("Invalid input");
+	} while (payment > ePtr->cash || payment < 0);
+	if (payment > 5){
+		payment -= 5;
+		strikes = 5;
+	}
+	else if (payment < 4)
+		strikes = 3;
+	else {
+		strikes = payment;
+		payment = 0;
+	}
+	int tStrikes = 0;
+	while (strikes > 0){
+		if (payment > 70)
+			odds = 70;
+		else 
+			odds = payment;
+		payment -= odds;
+		if (ePtr1->cur_hp < 1)
+			ePtr1->cooldown += 100;
+		int check = rand()%20;
+		if (ePtr->attributes[3]+check < ePtr1->attributes[3]+(rand()%20))
+			printf("%s misses.\n", ePtr->name.c_str());
+		check = rand()%ePtr->attributes[1];
+		check -= rand()%ePtr1->attributes[5];
+		check *= 2;
+		if (check < 0)
+			check = 0;
+		printf("%s deals %d damage.\n", ePtr->name.c_str(), check);
+		ePtr1->cur_hp -= check;
+		if (ePtr1->cur_hp < 1){
+			printf("%s has died.\n", ePtr1->name.c_str());
+			ePtr1->cooldown = 100;
+		}
+		if (rand()%100 < 29+odds)
+			strikes++;
+		strikes--;
+		tStrikes++;
+	}
+	ePtr->cooldown = 5*tStrikes;
 }
+
 void diceBomb(Entity* ePtr) {
-	return;
+	int payment, roll, check;
+	Entity* ePtr1;
+	do {
+		printf("How much gold do you pay to Weddench?\n >> ");
+		cin >> payment;
+		if (payment < 0 || payment > ePtr->cash)
+			printf("Invalid input");
+	} while (payment > ePtr->cash || payment < 0);
+	roll = rand()%20;
+	if (roll == 0){
+		ePtr->cur_hp -= 10;
+		ePtr->cooldown = 20;
+		return;
+	}
+	if (roll == 19){
+		printf("You hear laughter as Weddench smites your foes.\n");
+		for (int i = 0; i < ePtr->room->inRoom.size(); i++){
+			if (ePtr->room->inRoom[i]->npc){
+				ePtr->room->inRoom[i]->cur_hp = 0;
+				ePtr->room->inRoom[i]->cooldown = 100;
+			}
+		}
+		ePtr->cooldown = 20;
+		return;
+	}
+	if (roll < 8){
+		printf("Your dice roll fails.\n");
+		ePtr->cooldown = 20;
+		return;
+	}
+	else {
+		for (int i = 0; i < ePtr->room->inRoom.size(); i++){
+			if (ePtr->room->inRoom[i]->npc){
+				ePtr1 = ePtr->room->inRoom[i];
+				check = rand()%(ePtr->attributes[1]+payment);
+				check -= rand()%ePtr1->attributes[5];
+				check *= roll;
+				if (check < 0)
+					check = 0;
+				printf("%s deals %d damage to %s.\n", ePtr->name.c_str(), check, ePtr1->name.c_str());
+				ePtr1->cur_hp -= check;
+				if (ePtr1->cur_hp < 1){
+					printf("%s has died.\n", ePtr1->name.c_str());
+					ePtr->cooldown = 100;
+				}
+			}
+		}
+	}
+	ePtr->cooldown = 20;
 }
